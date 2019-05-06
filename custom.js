dcdb9dbe757746ad86a8b87570ff022514cfbc4e
jodavle <joda.lecu@gmail.com>
2019-05-05 20:30:21 -0600

    Mejoramiento de la página, usando BULMA

---
+++ custom.js
@@ -0,0 +1,26 @@
+function toggleClassMenu() {
+    myMenu.classList.add("menu--animatable");
+    if(!myMenu.classList.contains("menu--visible")) {
+        myMenu.classList.add("menu--visible");
+    } else {
+        myMenu.classList.remove('menu--visible');
+    }
+}
+
+function OnTransitionEnd() {
+    myMenu.classList.remove("menu--animatable");
+}
+
+var myMenu = document.querySelector(".menu");
+var oppMenu = document.querySelector(".menu-sandwich");
+myMenu.addEventListener("transitionend", OnTransitionEnd, false);
+oppMenu.addEventListener("click", toggleClassMenu, false);
+myMenu.addEventListener("click", toggleClassMenu, false);
+
+anime({
+	targets: '.nuevolanzamiento',
+	rotate: '1turn',
+	loop: true,
+	easing: 'linear',
+	duration: 10000,
+})

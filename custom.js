afa56df1a1c32e1c05de4e5c554e42b5b7c6389c
David <joda.lecu@gmail.com>
2019-05-09 11:41:50 -0600

    Esperando respuesta para terminar de codificar menú y futuros cambios

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+/*
 function toggleClassMenu() {
     myMenu.classList.add("menu--animatable");
     if(!myMenu.classList.contains("menu--visible")) {
@@ -16,9 +17,9 @@ var oppMenu = document.querySelector(".menu-sandwich");
 myMenu.addEventListener("transitionend", OnTransitionEnd, false);
 oppMenu.addEventListener("click", toggleClassMenu, false);
 myMenu.addEventListener("click", toggleClassMenu, false);
-
+*/
 anime({
-	targets: '.nuevolanzamiento',
+	targets: '.circle-data',
 	rotate: '1turn',
 	loop: true,
 	easing: 'linear',
